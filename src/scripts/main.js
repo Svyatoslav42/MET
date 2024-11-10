e0c3b1421c7294a32bced78e44912f2c15cdd8e8
Святослав <122892406+Svyatoslav-kolom@users.noreply.github.com>
2024-11-10 20:18:29 +0200

    edited styles

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,19 @@
+/* eslint-disable max-len */
 'use strict';
+
+document.querySelector('.icon--menu').addEventListener('click', function(event) {
+  event.preventDefault();
+  document.getElementById('menu').style.display = 'block';
+  document.documentElement.style.overflow = 'hidden';
+});
+
+document.querySelector('.icon--close').addEventListener('click', function(event) {
+  event.preventDefault();
+  document.getElementById('menu').style.display = 'none';
+  document.documentElement.style.overflow = '';
+});
+
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+  event.preventDefault();
+  this.reset();
+});
