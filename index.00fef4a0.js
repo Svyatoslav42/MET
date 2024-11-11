7028a4c0b58d9939a40f5260f5841da3b12a12b3
Святослав <122892406+Svyatoslav-kolom@users.noreply.github.com>
2024-11-11 12:51:41 +0200

    make build

---
+++ index.00fef4a0.js
@@ -0,0 +1,2 @@
+document.querySelector(".icon--menu").addEventListener("click",function(){document.documentElement.style.overflow="hidden"}),document.querySelector(".icon--close").addEventListener("click",function(){document.documentElement.style.overflow=""}),document.getElementById("contactForm").addEventListener("submit",function(e){e.preventDefault(),this.reset()});
+//# sourceMappingURL=index.00fef4a0.js.map
